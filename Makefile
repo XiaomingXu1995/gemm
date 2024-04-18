8d6c0370c0f86200533dfc5c46bcf48acbbc97c1
xiaomingxu1995 <xiaoming.xu@mail.sdu.edu.cn>
2024-04-17 23:34:33 -0400

    add multiplely execute gemm of small matrix to test the GFLOPS in gemm_float_multiple.cpp

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ all: exe1 exe2 exe3
 
 exe1: exe_generate_random exe_check 
 exe2: exe_gemm_base exe_gemm_base_1 exe_gemm_base_2 exe_gemm_base_3
-exe3: exe_gemm_float exe_gemm_float_mem_align
+exe3: exe_gemm_float exe_gemm_float_mem_align exe_gemm_float_multiple
 
 exe_generate_random: generate_random.cpp
 	g++ -O3 -g generate_random.cpp -o exe_generate_random
@@ -20,6 +20,8 @@ exe_gemm_float: gemm_float.cpp
 	g++ -O3 -g gemm_float.cpp -o exe_gemm_float -mavx2 -fopenmp
 exe_gemm_float_mem_align: gemm_float_mem_align.cpp
 	g++ -O3 -g gemm_float_mem_align.cpp -o exe_gemm_float_mem_align -mavx2 -fopenmp
+exe_gemm_float_multiple: gemm_float_multiple.cpp
+	g++ -O3 -g gemm_float_multiple.cpp -o exe_gemm_float_multiple -mavx2 -lpthread
 
 clean:
 	rm -rf exe_*
---
+++ gemm_float_multiple.cpp
@@ -0,0 +1,216 @@
+#include <iostream>
+#include <vector>
+#include <sys/time.h>
+#include <assert.h>
+#include <immintrin.h>
+#include <pthread.h>
+
+using namespace std;
+
+//const string src_file0 = "million0.file";
+//const string src_file1 = "million1.file";
+const string src_file0 = "billion0.float.random";
+const string src_file1 = "billion1.float.random";
+
+double get_sec(){
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return (double)tv.tv_sec + (double)tv.tv_usec / 1000000;
+}
+
+static void thread_bind(int cpu){
+	cpu_set_t cpu_set;
+	CPU_ZERO(&cpu_set);
+	CPU_SET(cpu, &cpu_set);
+	if(pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpu_set) != 0){
+		fprintf(stderr, "Error: cpu[%d] bind failed.\n", cpu);
+		exit(0);
+	}
+
+}
+
+void gemm_base_naive(float* a, float* b, float* c, int m, int n, int l){
+	for(int i = 0; i < m; i++){
+			for(int k = 0; k < l; k++){
+		for(int j = 0; j < n; j++){
+				c[i * n + j] += a[i * l + k] * b[k * n + j];
+			}
+		}
+	}
+}
+
+
+void gemm_base_vec(float* a, float* b, float* c, int m, int n, int l){
+	__m256 v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11;
+	//n == 24
+	
+	int s_i = 0;
+	int s_k = 0;
+	//n = 24;
+	//int k = l;
+
+	//#pragma omp parallel for num_threads(10)
+	for(int s_j = 0; s_j < n; s_j+=24){
+		for(int s_i = 0; s_i < m; s_i+=4){
+			__m256 v0 = _mm256_set1_ps(0.0);
+			__m256 v1 = v0; 
+			__m256 v2 = v0; 
+			__m256 v3 = v0; 
+			__m256 v4 = v0; 
+			__m256 v5 = v0; 
+			__m256 v6 = v0; 
+			__m256 v7 = v0; 
+			__m256 v8 = v0; 
+			__m256 v9 = v0; 
+			__m256 v10 = v0; 
+			__m256 v11 = v0; 
+			for(int kk = 0; kk < l; kk++){
+				__m256 v12 = _mm256_load_ps(&b[kk*n+s_j + 0]);
+				__m256 v13 = _mm256_load_ps(&b[kk*n+s_j + 8]);
+				__m256 v14 = _mm256_load_ps(&b[kk*n+s_j + 16]);
+
+				__m256 v15 = _mm256_set1_ps(a[(s_i+0)*l+kk]);
+				v0 = _mm256_add_ps(v0, _mm256_mul_ps(v15, v12));
+				v1 = _mm256_add_ps(v1, _mm256_mul_ps(v15, v13));
+				v2 = _mm256_add_ps(v2, _mm256_mul_ps(v15, v14));
+
+				v15 = _mm256_set1_ps(a[(s_i+1)*l+kk]);
+				v3 = _mm256_add_ps(v3, _mm256_mul_ps(v15, v12));
+				v4 = _mm256_add_ps(v4, _mm256_mul_ps(v15, v13));
+				v5 = _mm256_add_ps(v5, _mm256_mul_ps(v15, v14));
+
+				v15 = _mm256_set1_ps(a[(s_i+2)*l+kk]);
+				v6 = _mm256_add_ps(v6, _mm256_mul_ps(v15, v12));
+				v7 = _mm256_add_ps(v7, _mm256_mul_ps(v15, v13));
+				v8 = _mm256_add_ps(v8, _mm256_mul_ps(v15, v14));
+
+				v15 = _mm256_set1_ps(a[(s_i+3)*l+kk]);
+				v9 = _mm256_add_ps(v9, _mm256_mul_ps(v15, v12));
+				v10 = _mm256_add_ps(v10, _mm256_mul_ps(v15, v13));
+				v11 = _mm256_add_ps(v11, _mm256_mul_ps(v15, v14));
+			}
+			_mm256_store_ps(&c[(s_i+0)*n+s_j+0], v0);
+			_mm256_store_ps(&c[(s_i+0)*n+s_j+8], v1);
+			_mm256_store_ps(&c[(s_i+0)*n+s_j+16], v2);
+
+			_mm256_store_ps(&c[(s_i+1)*n+s_j+0], v3);
+			_mm256_store_ps(&c[(s_i+1)*n+s_j+8], v4);
+			_mm256_store_ps(&c[(s_i+1)*n+s_j+16], v5);
+
+			_mm256_store_ps(&c[(s_i+2)*n+s_j+0], v6);
+			_mm256_store_ps(&c[(s_i+2)*n+s_j+8], v7);
+			_mm256_store_ps(&c[(s_i+2)*n+s_j+16], v8);
+
+			_mm256_store_ps(&c[(s_i+3)*n+s_j+0], v9);
+			_mm256_store_ps(&c[(s_i+3)*n+s_j+8], v10);
+			_mm256_store_ps(&c[(s_i+3)*n+s_j+16], v11);
+		}
+	}
+
+}
+
+
+int main(int argc, char * argv[]){
+	double t0 = get_sec();
+	if(argc < 5){
+		cerr << "error, run as: <exe_gemm_base m n l res.file> to compute the (M,K) multiple (K,N) matrix, and store the result into res.file" << endl;
+		return 1;
+	}
+	int m = stoi(argv[1]);
+	int n = stoi(argv[2]);
+	int l = stoi(argv[3]);
+	string res_file = argv[4];
+
+	thread_bind(0);
+
+	//float* a = new float[m * l];
+	//float* b = new float[l * n];
+	//float * c1 = new float[m * n];
+	//float * c2 = new float[m * n];
+	float* a = (float*)_mm_malloc(m * l * sizeof(float), 64);
+	float* b = (float*)_mm_malloc(l * n * sizeof(float), 64);
+	float* c1 = (float*)_mm_malloc(m * n * sizeof(float), 64);
+	float* c2 = (float*)_mm_malloc(m * n * sizeof(float), 64);
+
+	FILE* fp0 = fopen(src_file0.c_str(), "rb");
+	FILE* fp1 = fopen(src_file1.c_str(), "rb");
+	assert(fp0 != NULL);
+	assert(fp1 != NULL);
+
+	int read_a = fread(a, sizeof(float), m * l, fp0);
+	assert(read_a == m * l);
+	fclose(fp0);
+	//for(int i = 0; i < m * l; i++){
+	//	cout << a[i] << endl;
+	//}
+	//cout << "----------------" << endl;
+
+	int read_b = fread(b, sizeof(float), l * n, fp1);
+	assert(read_b == l * n);
+	fclose(fp1);
+	//for(int i = 0; i < n * l; i++){
+	//	cout << b[i] << endl;
+	//}
+	//exit(0);
+
+	for(int i = 0; i < m * n; i++){
+		c1[i] = 0.0;
+		c2[i] = 0.0;
+	}
+	
+	//int** res = new int*[m];
+	//for(int i = 0; i < m; i++){
+	//	res[i] = new int[n];
+	//	for(int j = 0; j < n; j++){
+	//		res[i][j] = 0;
+	//	}
+	//}
+
+	long num_ops = 2L * m * n * l;
+	int loop_time = (int)(2e11 / num_ops);
+
+	double t1 = get_sec();
+	cerr << "======time of init matrix is: " << t1 - t0 << endl;
+
+	for(int i = 0; i < loop_time; i++){
+		gemm_base_vec(a, b, c2, m, n, l);
+	}
+
+	//gemm_base(matrix0, matrix1, res, m, n, l);
+
+	double t2 = get_sec();
+	cerr << "======time of warming up gemm_base_vec is: " << t2 - t1 << endl;
+	double gflops = (double)num_ops * 1e-9 / ((t2-t1)/loop_time);
+	fprintf(stderr, "the gflops of warming up gemm_base_vec matrix %d %d %d is: %lf\n", m, n, l, gflops); 
+
+	for(int i = 0; i < loop_time; i++){
+		gemm_base_vec(a, b, c2, m, n, l);
+	}
+
+	double t2_2 = get_sec();
+	cerr << "======time of gemm_base_vec is: " << t2_2 - t2 << endl;
+	double gflops_2 = (double)num_ops * 1e-9 / ((t2_2-t2)/loop_time);
+	fprintf(stderr, "the gflops of gemm_base_vec matrix %d %d %d is: %lf\n", m, n, l, gflops_2); 
+
+	gemm_base_naive(a, b, c1, m, n, l);
+
+	string res_file_base0 = res_file + ".base0";
+	FILE* fp_res = fopen(res_file_base0.c_str(), "wb");
+	assert(fp_res != NULL);
+	int err_num = 0;
+	for(int i = 0; i < m * n; i++){
+		if(abs(c1[i]-c2[i]) > 1e-5){
+			err_num++;
+			fprintf(fp_res, "%d\t%f\t%f\n", i, c1[i], c2[i]);
+		}
+	}
+	fclose(fp_res);
+
+	double t3 = get_sec();
+	cerr << "======time of check result gemm_base is: " << t3 - t2_2 << endl;
+	cerr << "-----the error number is: " << err_num << endl;
+
+	cerr << "finished!" << endl;
+
+	return 0;
+}
